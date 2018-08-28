5fc32a397dbfbc16f1bb7ca0f930491fa9a5de77
romcar <rommel.carrasquilla@gmail.com>
2018-08-28 11:33:01 -0400

    Add react-responsive-modal for simple designed modal

--- src/components/Events/Event/Event.jsx
+++ src/components/Events/Event/Event.jsx
@@ -1,7 +1,8 @@
+import Modal from 'react-responsive-modal';
+import moment from 'moment';
 import React, { Component } from 'react';
 import StyledEvent from '../../../assets/StyledComponents/Event/Event';
 import utils from '../../../assets/utils';
-import moment from 'moment';
 // styled components
 import StyledImage from '../../../assets/StyledComponents/Image/Image';
 import StyledEventInfo from '../../../assets/StyledComponents/EventInfo/EventInfo';
@@ -11,15 +12,23 @@ import StyledEventLocation from '../../../assets/StyledComponents/EventInfo/Even
 class Event extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isClicked: false
+    }
+
+    this.toggleEventModal = this.toggleEventModal.bind(this);
   }
 
   toggleEventModal() {
-    console.log('toggling');
+    const { isClicked } = this.state;
+    this.setState({ isClicked: !isClicked });
   }
 
 
   render() {
     const { event } = this.props;
+    const { isClicked } = this.state;
     const convert = utils.convertSpecialCharsToString;
 
     return (
@@ -35,10 +44,15 @@ class Event extends Component {
             <br />
             <span className="event-desc">{event.description ? convert(event.description.substring(0, 200)) : 'There is no description for this event, sorry'}</span>
           </div>
+          <Modal open={isClicked} onClose={this.toggleEventModal} center>
+            <h2>Test</h2>
+          </Modal>
         </StyledEventInfo>
       </StyledEvent>
     );
   }
 }
+
+// This could possibly be broken up into further components
 
 export default Event;
