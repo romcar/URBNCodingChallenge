78b99e0dbbead7bcf77d60ca76c651ee80a954dc
romcar <rommel.carrasquilla@gmail.com>
2018-08-28 11:04:34 -0400

    Add fake information for testing

---
+++ fakeSeed.js
@@ -0,0 +1,91 @@
+module.exports = [{
+  "watching_count": null,
+  "olson_path": "America/New_York",
+  "calendar_count": null,
+  "comment_count": null,
+  "region_abbr": "PA",
+  "postal_code": "19131",
+  "going_count": null,
+  "all_day": "0",
+  "latitude": "39.9833256",
+  "groups": null,
+  "url": "this/is/the/url/for/the/event",
+  "id": "E0-001-1234567890-9",
+  "privacy": "1",
+  "city_name": "Philadelphia",
+  "link_count": null,
+  "longitude": "-75.2229687",
+  "country_name": "United States",
+  "country_abbr": "USA",
+  "region_name": "Pennsylvania",
+  "start_time": "2018-09-09 19:00:00",
+  "tz_id": null,
+  "description": "There once was a description here but no more. Actually there was no description and a story had to be told. He picked up the watter bottle, took a sip, and put it down. Then immediately took a drag of his cigarette to calm his nerves. The sun set in the east and the moon came up shortly after. I hope this is 200 + characters. Let's find out.",
+  "modified": "2018-05-01 17:53:35",
+  "venue_display": "1",
+  "tz_country": null,
+  "performers": {
+    "performer": [
+      {
+        "creator": "girlillamarketing",
+        "linker": "evdb",
+        "name": "Sugarland",
+        "url": "http://concerts.eventful.com/performers/sugarland-/P0-001-000009665-3?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+        "id": "P0-001-000009665-3",
+        "short_bio": "Country / Pop"
+      },
+      {
+        "creator": "BBRMusicGroup",
+        "linker": "evdb",
+        "name": "Lindsay Ell",
+        "url": "http://concerts.eventful.com/Lindsay-Ell?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+        "id": "P0-001-000140403-5",
+        "short_bio": "Stoney Creek Recording Artist Linday Ell"
+      },
+      {
+        "creator": "kateroeder",
+        "linker": "evdb",
+        "name": "Frankie Ballard",
+        "url": "http://concerts.eventful.com/Frankie-Ballard?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+        "id": "P0-001-000215447-4",
+        "short_bio": "Country"
+      }
+    ]
+  },
+  "title": "Sugarland",
+  "venue_address": "5201 Parkside Avenue",
+  "geocode_type": "EVDB Geocoder",
+  "tz_olson_path": null,
+  "recur_string": null,
+  "calendars": null,
+  "owner": "evdb",
+  "going": null,
+  "country_abbr2": "US",
+  "image": {
+    "small": {
+      "width": "48",
+      "url": "http://d1marr3m5x4iac.cloudfront.net/images/small/I0-001/003/948/039-3.jpeg_/sugarland-39.jpeg",
+      "height": "48"
+    },
+    "width": "48",
+    "caption": null,
+    "medium": {
+      "width": "128",
+      "url": "http://d1marr3m5x4iac.cloudfront.net/images/medium/I0-001/003/948/039-3.jpeg_/sugarland-39.jpeg",
+      "height": "128"
+    },
+    "url": "http://d1marr3m5x4iac.cloudfront.net/images/small/I0-001/003/948/039-3.jpeg_/sugarland-39.jpeg",
+    "thumb": {
+      "width": "48",
+      "url": "http://d1marr3m5x4iac.cloudfront.net/images/thumb/I0-001/003/948/039-3.jpeg_/sugarland-39.jpeg",
+      "height": "48"
+    },
+    "height": "48"
+  },
+  "created": "2018-03-05 20:30:22",
+  "venue_id": "V0-001-000282568-8",
+  "tz_city": null,
+  "stop_time": null,
+  "venue_name": "Mann Center for the Performing Arts",
+  "venue_url": "http://philadelphia.eventful.com/venues/mann-center-for-the-performing-arts-/V0-001-000282568-8?utm_source=apis&utm_medium=apim&utm_campaign=apic"
+}];
