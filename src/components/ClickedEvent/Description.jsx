94587751c0fb04174455d24dd7c41be78309bfac
romcar <rommel.carrasquilla@gmail.com>
2018-08-29 15:12:33 -0400

    Fix typo even-> event

--- src/components/ClickedEvent/Description.jsx
+++ src/components/ClickedEvent/Description.jsx
@@ -4,7 +4,7 @@ import StyledDescription from '../../assets/StyledComponents/ModalDescription/Mo
 export default (props) => {
   return (
     <StyledDescription>
-      {props.desc || "This even does not have a description. Click on the venue name to go get more information"}
+      {props.desc || "This event does not have a description. Click on the venue name to go get more information"}
     </StyledDescription>
   )
 }
